b5bce1db947c71f83e07ff76c9671dc6ccd9119d
theedu007 <eabigboy4@gmail.com>
2018-08-12 23:38:32 -0600

    punto 1 guia

---
+++ GUIA 3 UDB/Investigacion_Complementaria/variables_variable.php
@@ -0,0 +1,24 @@
+<?php
+    /*Las variables variables son nombres de variables que se pueden definir y usar dinámicamente,
+    una variable variable toma el valor de una variable y lo trata como el nombre de una variable*/
+
+//Ejemplo practico
+
+$nombre = (string) 'Eduardo';
+$apellido = (string) 'Arevalo';
+$edad = (integer) 27;
+
+echo "Nombre: $nombre <br>";
+echo "apellido: $apellido <br>";
+echo "Edad: $edad <br>";
+
+echo "<br>";
+
+$campos = array ('Nombre', 'Apellido', 'Edad');
+$info = array ('Eduardo', 'Arevalo', '27');
+
+for ($i=0; $i < 3 ; $i++)
+{
+    echo "$campos[$i] : $info[$i] <br>"
+}
+?>
