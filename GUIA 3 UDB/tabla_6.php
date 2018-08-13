4cf341ea742f4fca676ae9e0a445c389ee727bfa
theedu007 <eabigboy4@gmail.com>
2018-08-12 22:43:11 -0600

    final

--- GUIA 3 UDB/tabla_6.php
+++ GUIA 3 UDB/tabla_6.php
@@ -1,9 +1,7 @@
 <?php
-$primer_string = (string) 'Roberto';
-$segundo_string = (string) ' Martinez';
+$primer_string = (string)'ROBERTO';
+$segundo_string = (string) ' MARTINEZ';
 $primer_entero = (integer) 27;
-
 echo 'Resultado de concatenar dos variables string: '.$primer_string.$segundo_string.'<br>';
-    
-echo 'Resultado de concatenar una variable string e integer: '$segundo_string.$primer_entero.<br>;
+echo 'Resultado de concatenar una variable string e integer: '.$segundo_string.$primer_entero.'<br>';
 ?>
