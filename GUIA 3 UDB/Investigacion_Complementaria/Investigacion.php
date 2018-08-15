919907fe9fa09b27a0556d1fca16d85ea48fcd6d
theedu007 <eabigboy4@gmail.com>
2018-08-15 04:21:10 -0600

    correciones investigacion
    correcciones de codigo

--- GUIA 3 UDB/Investigacion_Complementaria/Investigacion.php
+++ GUIA 3 UDB/Investigacion_Complementaria/Investigacion.php
@@ -19,37 +19,37 @@
             echo "$frase[0]<br>";
             echo "$frase[1]<br>";
             echo "$frase[2]<br>";
-            
+            echo "<br>";
             echo "fopen: Escribe un texto con formato a un destino especificado<br>"
             $dir = fopen("/var/www/guias_udb/txt/archivo","w");
-            fprintf($dir,'%s'"hola");
-        
+            fprintf($dir,'%s',"hola");
+            echo "<br>";
             echo "htmlspecialchars — Convierte caracteres especiales a entidades HTML <br>";
             $nuevo = htmlspecialchars("<a href='test'>Test</a>", ENT_QUOTES);
             echo $nuevo; // &lt;a href=&#039;test&#039;&gt;Test&lt;/a&gt;
-            
+            echo "<br>";
             echo "md5 Calcula el 'hash' md5 de un string <br>";
             echo md5("hola");
-        
+            echo "<br>";
             echo "money_format: Da formato a un número como un string de moneda<br>";
             $number = 1234.56;
             setlocale(LC_MONETARY,"en_US");
             echo money_format("El precio es %i", $number);
             echo "<br>";
-        
+            echo "<br>";
             echo "number_format: Formatear un número con los millares agrupados";
             $num = 10.95777777;
             echo number_format($num,2);
             echo "<br>";
-            
+            echo "<br>";
             echo "str_replace: Reemplaza todas las apariciones del string buscado con el string de reemplazo <br>";
             $phrase  = "Deberias comer frutas, verduras y fibra.";
-            echo "$phrase <br>"
+            echo "$phrase <br>";
             $healthy = array("frutas", "verduras", "fibra");
             $yummy   = array("pizza", "cerveza", "helado");
             $newphrase = str_replace($healthy, $yummy, $phrase);
             echo "$newphrase <br>";
-        
+            echo "<br>";
             echo "strcmp: Comparación de string segura a nivel binario<br>";
             $var1 = "Hola";
             $var2 = "hola";
@@ -57,37 +57,37 @@
                 echo '$var1 no es igual a $var2 en una comparación que considera mayúsculas y minúsculas';
             }
             echo "<br>";
-        
+            echo "<br>";
             echo "strlen: Obtiene la longitud de un string <br>";
             $str = 'abcdef';
             echo "$str <br>";
             echo strlen($str);
-            
+            echo "<br>";
             echo "strncmp: Comparación segura a nivel binario de los primeros n caracteres entre strings <br>";
             $str1 = "phpaaa";
             $str2 = "php";
             echo "textos a comparar: $str1 y $str2 <br>";
             echo strcmp($str1, $str2); // 3
             echo "<br>";
-        
+            echo "<br>";
             echo "strpos — Encuentra la posición de la primera ocurrencia de un substring en un string <br>";
             $mystring = 'abc';
             $findme   = 'a';
             echo "texto donde se buscara: $mystring, caracter que se buscara: $findme <br>";
             $pos = strpos($mystring, $findme);
-            
+            echo "<br>";  
             echo "strtoupper: Convierte un string a mayúsculas <br>";
             $texto = 'hola';
             echo "$texto <br>";
             echo strtoupper($texto);
             echo "<br>";
-            
+            echo "<br>";
             echo "strtolower: Convierte una cadena a minúsculas <br>";
             $ejemplo = 'HOLA';
             echo "$ejemplo <br>";
             echo strtolower($ejemplo);
             echo "<br>";
-        
+            echo "<br>";
             echo "substr_replace: Reemplaza el texto dentro de una porción de un string <br>";
             $var = 'ABCDEFGH:/MNRPQR/';
             echo "Original: $var<hr />\n"; 
@@ -96,19 +96,19 @@
             echo substr_replace($var, 'bob', 0) . "<br />\n";
             echo substr_replace($var, 'bob', 0, strlen($var)) . "<br />\n";
             echo "<br>";
-        
+            echo "<br>";
             echo "trim: Elimina espacio en blanco (u otro tipo de caracteres) del inicio y el final de la cadena <br>";
             $hello  = "Hello World";
             echo "$hello <br>";
             $trimmed = trim($hello, 'HdWr');
             echo "$trimmed <br>";
-            
+            echo "<br>";
             echo "cwords: Convierte a mayúsculas el primer caracter de cada palabra de una cadena";
             $foo = 'hello world!';
             echo "$foo <br>";
             $foo = ucwords($foo);             // Hello World!
             echo "$foo <br>";
-        
+            echo "<br>";
             echo "wordwrap: Ajusta un string hasta un número dado de caracteres <br>";
             $text = "The quick brown fox jumped over the lazy dog.";
             echo "$text <br>";
