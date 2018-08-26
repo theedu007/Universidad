4a5a47c6124dbc596f03fd5c1c7ac7b5116717d5
theedu007 <eabigboy4@gmail.com>
2018-08-26 15:34:30 -0600

    Segundo Ejercicio
    Segundo Ejercicio Parcial PHP

---
+++ Parcial_PHP_Lab_AM150283/Segundo Ejercicio/Procedimiento.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Salario Total</title>
+</head>
+
+<body>
+    <h2>El desgloce de su salario es: </h2>
+    <?php
+    require 'calculo_renta.php';
+    
+    $salarioBase = $_GET['salario'];
+    $AFP = $salarioBase * 0.0725;
+    $ISSS = $salarioBase * 0.03;
+    $salarioConDescuentos = $salarioBase - ($AFP + $ISSS);
+    $renta = calculoRenta($salarioConDescuentos);
+    $salarioTotal = $salarioConDescuentos - $renta;
+    
+    echo("Su salario base es: $salarioBase <br>");
+    echo("Descuento AFP: $AFP <br>");
+    echo("Descuento ISSS: $ISSS <br>");
+    echo("Salario con descuentos: $salarioConDescuentos <br>");
+    echo("Renta: $renta <br>");
+    echo("Salario Total: $salarioTotal")
+    ?>
+</body>
+</html>
---
+++ Parcial_PHP_Lab_AM150283/Segundo Ejercicio/calculo_renta.php
@@ -0,0 +1,25 @@
+<?php
+function calculoRenta($salarioConDescuentos)
+{
+    $base;
+    if($salarioConDescuentos > 0.01 || $salarioConDescuentos < 472)
+    {
+        return (0);
+    }
+    else if($salarioConDescuentos > 472.01 || $salarioConDescuentos < 895.24)
+    {
+        $excesoPorPorcentaje = ($salarioConDescuentos - 472) * 0.1;
+        return ($excesoPorPorcentaje + 17.67);
+    }
+    else if($salarioConDescuentos > 895.25 || $salarioConDescuentos < 2038.10)
+    {
+        $excesoConPorcentaje = ($salarioConDescuentos - 895.24) * 0.2;
+        return ($excesoPorPorcentaje + 60);
+    }
+    else if($salarioConDescuentos > 2038.11)
+    {
+        $excesoPorPorcentaje = ($salarioConDescuentos - 2038.10) * 0.3;
+        return ($excesoPorPorcentaje + 288.57);
+    }
+}
+?>
