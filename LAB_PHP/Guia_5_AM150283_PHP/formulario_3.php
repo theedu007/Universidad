9b22a0803ebaa4a0eda3365b077948b4d251e13d
theedu007 <eabigboy4@gmail.com>
2018-09-08 20:37:43 -0600

    aregando formulario 3

---
+++ LAB_PHP/Guia_5_AM150283_PHP/formulario_3.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+    <title>Ejemplo 2</title>
+</head>
+<body>
+    <form action="recibir_datos.php" method="post" name="datos">
+        <table>
+            <tr>
+                <td colspan="2" align="center"><h3>Datos Personales</h3></td>
+            </tr>
+            
+            <tr>
+                <td>Nombre: </td>
+                <td><input type="text" name="frmNombre" size="25" maxlength="25"></td>
+            </tr>
+            
+            <tr>
+                <td>Apellido: </td>
+                <td><input type="text" name="frmApellido" size="25" maxlength="25"> </td>
+            </tr>
+            
+            <tr>
+                <td>Sexo:</td>
+                <td>
+                    <input type="radio" name="frmgen" value="masculino">Masculino
+                    <input type="radio" name="frmgen" value="femenino">Femenino
+                </td>
+            </tr>
+            
+            <tr>
+                <td>Edad: </td>
+                <td><input type="text" name="frmEdad" size="3" maxlength="3"> </td>
+            </tr>
+            
+            <tr>
+                <td>Pais</td>
+                <td>
+                    <select name="frmPais" size="1">
+                        <option value='ES'>El Salvador</option>
+                        <option value='GU'>Guatemala</option>
+                        <option value='CR'>Costa Rica</option>
+                        <option value='HO'>Honduras</option>
+                        <option value='NI'>Nicaragua</option>
+                        <option value='BE'>Belice</option>
+                    </select>
+                </td>
+            </tr>
+            
+            <tr>
+                <td>Cursos a tomar:</td>
+                <td>
+                    <select name="frmCursos[]" size="6" multiple>
+                        <option value='Literatura'>Literatura</option>
+                        <option value='Programacion'>Programacion</option>
+                        <option value='Matematicas'>Matematicas</option>
+                        <option value='Ciencias'>Ciencias</option>
+                    </select>
+                </td>
+            </tr>
+            
+        </table>
+    </form>
+</body>
+</html>
---
+++ LAB_PHP/Guia_5_AM150283_PHP/recibir_datos.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+    <?php
+    echo "<center><h3>Datos Ingresados</h3></center>";
+    echo "<table border=\"1\" align=\"center\" bgcolor=\"#AACC66\">";
+    echo "<tr bgcolor=\"#669966\">";
+    echo "<th>";
+    echo "Nombre de variable <br>";
+    echo "de formulario";
+    echo "</th>";
+    echo "<th>";
+    echo "Valor introducido";
+    echo "</th>";
+    echo "</tr>";
+    foreach($_POST as $nombre_campo => $valor)
+    {
+        echo "<tr><td> $nombre_campo </td>";
+        echo "<td>";
+        if(gettype($valor)=="array")
+        {
+            foreach($valor as $elemento)
+                echo "$elemento <br>";
+        }
+        else
+        {
+            echo $valor;
+            echo "</td>";
+            echo "</td>";
+        }
+    }
+    echo "</table><br>";
+    ?>
+</body>
+</html>
